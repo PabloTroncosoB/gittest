ecee68fdb39179251d6b103e00ef9a2fff435283
PabLouD <pablord@gmail.com>
2013-03-27 18:07:24 -0400

    git - rm alpha release

--- excel/alpha_release/antiguo cyt/index.php
+++
@@ -1,9 +0,0 @@
-<?
-$nombre = "temp";
-?>
-<div id="formulario" style="padding: 15 15 15 15 ;alling:center;border-color:orange;border-style:solid;border-width:thin;display:block;width:310;background-color:#FFFFCC" >
-<form method='POST' enctype='multipart/form-data' action='subir2.php'>
-<input type='hidden' name='nombre' value='temp'>
-Subir: <input type='file' name='archivo' size='20' accept="application/vnd.ms-excel">
-<input type='submit' value='Enviar'>
-</form>
